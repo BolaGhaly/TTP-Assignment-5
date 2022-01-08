4bfa1bdfa572da7fa40743ac5da45b30a8dd76af
Bola Ghaly <bola.ghaly@cix.csi.cuny.edu>
2022-01-08 00:06:12 -0500

    Updated index.html, style.css, and script.js. Closes #3, Closes #6.

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-//Flexbox Container
+//--------------------------------- Flexbox Container ---------------------------------
 const flexbox_container = document.querySelector(".flexbox-container");
-const allCols = document.querySelectorAll(".col");
-const allRows = document.querySelectorAll(".row");
+const allColsArr = Array.from(document.querySelectorAll(".col"));
+const allRowsArr = document.querySelectorAll(".row");
 
-// Buttons
+//------------------------------ Center Buttons (Rows + Columns) ----------------------
 const addRowButton = document.querySelector(".add-row");
 const removeRowButton = document.querySelector(".remove-row");
 const resetButton = document.querySelector(".reset");
 const addColButton = document.querySelector(".add-col");
 const removeColButton = document.querySelector(".remove-col");
 
-// addRowButton.onclick = function () {
-
-// }
-
-// removeRowButton.onclick = function () {
-
-// }
-
+//Closes issue #3
 resetButton.onclick = function () {
   location.reload();
 };
+
+//------------------- Form Colors + Submit Buttons ------------------------------------
+const first_form_color = document.querySelector("#first-form-color");
+const second_form_color = document.querySelector("#second-form-color");
+const third_form_color = document.querySelector("#third-form-color");
+const fourth_form_color = document.querySelector("#fourth-form-color");
+
+const second_form_submit = document.querySelector(".second-form-submit");
+console.log(second_form_submit);
+
+const third_form_submit = document.querySelector(".third-form-submit");
+console.log(third_form_submit);
+
+const fourth_form_submit = document.querySelector(".fourth-form-submit");
+console.log(fourth_form_submit);
+
+
+//Closes issue #6
+allColsArr.map((element) => {
+  element.addEventListener("click", function () {
+    element.style.backgroundColor = first_form_color.value;
+  });
+});
