cd7981ea0c3769932a366e2c84b8d5b82590b727
Bola Ghaly <bola.ghaly@cix.csi.cuny.edu>
2022-01-08 03:19:25 -0500

    Updated script.js. Closes #2, Closes #4

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //--------------------------------- Flexbox Container ---------------------------------
-const flexbox_container = document.querySelector(".flexbox-container");
+let flexbox_container = document.querySelector(".flexbox-container");
 const allColsArr = Array.from(document.querySelectorAll(".col"));
-const allRowsArr = document.querySelectorAll(".row");
+const allRowsArr = Array.from(document.querySelectorAll(".row"));
 
 //------------------------------ Center Buttons (Rows + Columns) ----------------------
 const addRowButton = document.querySelector(".add-row");
@@ -10,9 +10,21 @@ const resetButton = document.querySelector(".reset");
 const addColButton = document.querySelector(".add-col");
 const removeColButton = document.querySelector(".remove-col");
 
+//Closes issue #2
+removeRowButton.onclick = function () {
+  flexbox_container.removeChild(allRowsArr.pop());
+};
+
 //Closes issue #3
 resetButton.onclick = function () {
   location.reload();
+};
+
+//Closes issue #4
+removeColButton.onclick = function () {
+  allRowsArr.map((element) => {
+    element.lastElementChild.remove();
+  });
 };
 
 //------------------- Form Colors + Submit Buttons ------------------------------------
@@ -29,7 +41,6 @@ console.log(third_form_submit);
 
 const fourth_form_submit = document.querySelector(".fourth-form-submit");
 console.log(fourth_form_submit);
-
 
 //Closes issue #6
 allColsArr.map((element) => {
