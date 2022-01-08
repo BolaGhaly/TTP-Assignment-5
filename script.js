290a8e00b7615ccfc6c379852244deb4d120c905
Bola Ghaly <bola.ghaly@cix.csi.cuny.edu>
2022-01-07 21:13:02 -0500

    Updated index.html and Added style.css

---
+++ script.js
@@ -0,0 +1,23 @@
+//Flexbox Container
+const flexbox_container = document.querySelector(".flexbox-container");
+const allCols = document.querySelectorAll(".col");
+const allRows = document.querySelectorAll(".row");
+
+// Buttons
+const addRowButton = document.querySelector(".add-row");
+const removeRowButton = document.querySelector(".remove-row");
+const resetButton = document.querySelector(".reset");
+const addColButton = document.querySelector(".add-col");
+const removeColButton = document.querySelector(".remove-col");
+
+// addRowButton.onclick = function () {
+
+// }
+
+// removeRowButton.onclick = function () {
+
+// }
+
+resetButton.onclick = function () {
+  location.reload();
+};
